4f760527be77f4f92b2c5602c745456486aa145b
Dan Shahin <dan@codescience.com>
2015-02-06 11:16:32 -0800

    fix gulpfile to watch for changes

--- app/app.js
+++ app/app.js
@@ -16,3 +16,4 @@ angular.module('workTimeTrackerApp', ['ui.bootstrap', 'ngRoute', 'ngAnimate']).c
       redirectTo: '/'
     });
 }]);
+ 
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
+var watch = watch = require('gulp-watch');
+var paths = gulp.paths;
+
 
 // Static server
 gulp.task('serve', function() {
     browserSync({
         server: {
-            baseDir: "./"
+            baseDir: "."
         }
     });
 });
 
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('default', ['serve'], function () {
+  gulp.watch('app/**/*.js',   ['bs-reload']);
+  gulp.watch('css/*.css',  ['bs-reload']);
+  gulp.watch('*.html', ['bs-reload']);
+});
